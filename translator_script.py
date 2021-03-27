5df663c01aa3845bed697358e5650f78457c5bc9
medbenzekri <medbenzekri219@gmaim.com>
2021-03-27 17:00:53 +0100

    excutable add and progressbar added

--- translator_script.py
+++ translator_script.py
@@ -1,10 +1,25 @@
 import pysubs2
 import sys
 from googletrans import Translator
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+
+
 arg=sys.argv[0]
 translator = Translator()
 subs= pysubs2.load(sys.argv[1], encoding="utf-8")
-for line in subs :
+l=len(subs)
+for i,line in enumerate(subs) :
 	if line.text.find("{") == -1   :
 		if line.text.find(r"\N")== -1 :
 		    line.text = translator.translate(line.text,dest='ar').text
@@ -14,6 +29,8 @@ for line in subs :
 	else :
 	    pos = line.text.find("}")
 	    line.text = translator.translate(line.text[pos+1:],dest='ar').text
+	printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'subbed', length = 50)
 
 subs.save(sys.argv[2])
+
 
